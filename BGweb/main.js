1b18b56c17c650cbb93379a27e334f1223e8f185
BryanGarcia3B <56742984+bryang10@users.noreply.github.com>
2021-03-16 22:37:31 -0500

    Add files via upload

---
+++ BGweb/main.js
@@ -0,0 +1,5 @@
+document.querySelector(' .menu-boton' ).addEventListener(' click' , () => {document.querySelector('.nav-menu' ).classList.toggle('show');});
+ScrollReveal().reveal('.showcase');
+ScrollReveal().reveal('.new-cards',{delay:500});
+ScrollReveal().reveal('.cards-banner-one',{delay:500});
+ScrollReveal().reveal('.cards-banner-two',{delay:500});
